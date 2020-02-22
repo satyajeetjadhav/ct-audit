28db5b821e1c85b8b0b6653c67da9e179ced5c41
Satyajeet Jadhav <satyajeet@clevertap.com>
2020-02-22 20:57:25 +0530

    support for eds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LogParserService } from './log-parser.service';
 import { LogParserIosService } from './log-parser-ios.service'
 import { FormBuilder } from '@angular/forms';
+import * as Papa from 'papaparse';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ export class AppComponent {
   result;
   metadata;
   queue;
+  EDSMap = new Map();
 
   constructor(
     private logParser: LogParserService,
@@ -56,11 +58,9 @@ export class AppComponent {
     for (let line of logLines) {
       if (line.includes('New event processed:')) {
         return true;
-        break;
       }
       else if (line.includes('Queued Event:')) {
         return false;
-        break;
       }
       else {
         continue;
@@ -76,5 +76,69 @@ export class AppComponent {
     return 'https://' + region + '.dashboard.clevertap.com/' + accountId + '/' + CTID + '/profile-view.html';
   }
 
+  onFileLoad(fileLoadedEvent) {
+    // const textFromFileLoaded = fileLoadedEvent.target.result;
+    // this.csvContent = textFromFileLoaded;
+    // console.log(this.csvContent);
+    // alert(this.csvContent);
 
+    let currentlyProcessingEvent = '';
+    let self = this;
+    Papa.parse(fileLoadedEvent.target.result, {
+      worker: true,
+      step: function (row) {
+        console.log("Row:", row.data);
+        // get the index of Events Name
+        // get the index of Events Property
+        let iName = 1;
+        let iProperty = 2;
+        if (row.data[iName] !== '') {
+          currentlyProcessingEvent = row.data[iName];
+          self.EDSMap.set(row.data[iName], [row.data[iProperty]]);
+        } else {
+          self.EDSMap.get(currentlyProcessingEvent).push(row.data[iProperty]);
+        }
+      },
+      complete: function () {
+        console.log("All done!");
+        console.log(self.EDSMap);
+      }
+    });
+  }
+
+  onFileSelect(input: HTMLInputElement) {
+
+    const files = input.files;
+
+    if (files && files.length) {
+      /*
+       console.log("Filename: " + files[0].name);
+       console.log("Type: " + files[0].type);
+       console.log("Size: " + files[0].size + " bytes");
+       */
+
+      const fileToRead = files[0];
+
+      const fileReader = new FileReader();
+      fileReader.onload = this.onFileLoad.bind(this);
+
+      fileReader.readAsText(fileToRead, "UTF-8");
+    }
+
+
+  }
+
+  isEventInEDS(eventName: String) {
+    return this.EDSMap.has(eventName);
+  }
+
+  isEventPropertyInEDS(eventName: String, eventProperty: String) {
+    if (this.EDSMap.has(eventName)) {
+      if (this.EDSMap.get(eventName).indexOf(eventProperty) > -1) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
 }
